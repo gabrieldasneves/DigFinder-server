290c4afc65cd878a8669067ec38bcdadf18662c3
gabrieldasneves <galvesdasneves@gmail.com>
2025-11-11 22:44:47 +0900

    Refactor registers routes for improved readability
    - Renamed the router variable to `registersRoutes` for clarity.
    - Maintained existing route handlers while enhancing code consistency.

--- src/routes/registers.ts
+++ src/routes/registers.ts
@@ -2,11 +2,11 @@ import { Router } from "express";
 
 import { RegistersController } from "@/controllers/registers-controller";
 
-const router = Router();
+const registersRoutes = Router();
 const registersController = new RegistersController();
 
-router.get("/", registersController.listAllRegisters);
-router.get("/category/:category_id", registersController.index);
-router.get("/:id", registersController.show);
+registersRoutes.get("/", registersController.listAllRegisters);
+registersRoutes.get("/category/:category_id", registersController.index);
+registersRoutes.get("/:id", registersController.show);
 
-export { router };
+export { registersRoutes };
